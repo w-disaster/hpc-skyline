ada370c908d09f1a7c61d3ae416e04dd5de3b284
w-disaster <luca.fabri@studio.unibo.it>
2021-07-16 19:20:13 +0200

    add test7 subversion 2 of v1

--- src/cuda/version1/cuda-skyline-v1-2.cu
+++ src/cuda/version1/cuda-skyline-v1-2.cu
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <omp.h>
 #include "lib/hpc.h"
+#include <cuda_runtime.h>
 
 #define LINE_LENGHT 4000
 #define WARP_SIZE 32
@@ -28,10 +30,10 @@ double* build_matrix(FILE* fd, int* N, int* D){
     sscanf(n, "%d", N);
     printf("%d\n", *N);
 
-    /* Allocate the matrix (N x D), where each line i contains the values
+    /* Allocate the matrix (D x N), where each line i contains the values
 	   of the points on that dimension i.
 	*/
-    double *matrix = (double*) malloc((*N) * (*D) * sizeof(double));
+    double *matrix = (double*) malloc((*D) * (*N) * sizeof(double));
 	
     char* str;
     const char* s = " ";
@@ -51,48 +53,33 @@ double* build_matrix(FILE* fd, int* N, int* D){
     return matrix;
 }
 
-/* Returns true if the array s dominates the array d. 
- * Parameters:
- * - s, d: arrays of double
- * - length: number of elements of s and d 
- */
-__device__ bool dominance(double *s, double *d, int length){
-    bool strictly_minor = false;
-    bool strictly_major = false;
-    for(int i = 0; i < length && !strictly_minor; i++){
+/* Returns true if s dominates d */
+__device__ bool dominance(double *s, double *d, int dim){
+    bool weakly_major = true;
+    bool stricly_major = false;
+    for(int i = 0; i < dim && weakly_major; i++){
         if(s[i] < d[i]){
-			 strictly_minor = true;
+			 weakly_major = false;
 		}
         if(s[i] > d[i]){
-			strictly_major = true;
+			stricly_major = true;
 		}
     }
-    return !strictly_minor && strictly_major;
+    return weakly_major && stricly_major;
 }
 
-/* Kernel function:
- * each thread has the purpose to determine if the number in charge is in
- * the Skyline set. To do so, this function iterates on all the points and stops
- * if any of them dominates it.
- * The result, in the end, is put in the array S, stored in the global memory. 
- */
+/* Kernel function */
 __global__ void skyline(double *points, int *S, int n, int d){
 	const int y = blockIdx.y * blockDim.y + threadIdx.y;
 	if(y < n){
-		/* Copy the number in charge to the local memory in order
-		   to perform coalesced memory accesses
-		*/
-		double num[MAX_DIM];
-		memcpy(num, &points[y * d], d * sizeof(double));
-
 		int is_skyline_point = 1;
 		for(int i = 0; i < n && is_skyline_point; i++){
 			/* If num is dominates by another number then it is not
 			   in the Skyline set
 			*/
 			if(i != y){
-				if(dominance(&points[i * d], num, d)){
-					is_skyline_point = 0;						 
+				if(dominance(&points[i * d], &points[y * d], d)){
+						is_skyline_point = 0;                                            
 				}
 			}
 		}
@@ -121,41 +108,66 @@ int main(int argc, char* argv[]){
 	cudaSafeCall(cudaMalloc((void**)&d_S, (*N) * sizeof(int)));	
 
 	/* Define the block and grid dimensions */
-	dim3 block(1, WARP_SIZE * 2);
-	dim3 grid(1, ((*N) + WARP_SIZE * 2 - 1)/(WARP_SIZE * 2));
+	double avg[1024/32] = {0};
+	int num_blocks;
+
+	int device;
+	int maxActiveBlocks;
+	cudaDeviceProp props;
+	cudaGetDevice(&device);
+	cudaGetDeviceProperties(&props, device);
+
+	for(int k = 0; k < 10; k++){
+		for(int i = 1; i <= 32; i++){
+			int y_dim = i * 32;
+			dim3 block(1, y_dim);
+			//printf("%d\n", ((*N) + y_dim - 1)/y_dim);
+			dim3 grid(1, ((*N) + y_dim - 1)/y_dim);
+			
+			/* - Kernel function call to determine the Skyline set
+			- Wait it completion to calculate the time spent 
+			- Check for errors occurred in the GPU
+			 */
 		
-	/* - Kernel function call to determine the Skyline set
-	   - Calculate the time spent 
-	   - Check for errors occurred in the GPU
-	*/
-	cudaEvent_t t_kernel_start, t_kernel_stop;
-	cudaEventCreate(&t_kernel_start);
-	cudaEventCreate(&t_kernel_stop);	
-	
-	cudaEventRecord(t_kernel_start);
-	skyline<<<grid, block>>>(d_points, d_S, *N, *D);
-	cudaEventRecord(t_kernel_stop);
-	cudaCheckError();	
-	
-	/* - Allocate space on heap (host memory) in order to store the result
+			double t_kernel_start = hpc_gettime();	
+			skyline<<<grid, block>>>(d_points, d_S, *N, *D);
+			cudaDeviceSynchronize();
+			double t_kernel_end = hpc_gettime();
+
+			cudaOccupancyMaxActiveBlocksPerMultiprocessor(&maxActiveBlocks, skyline, y_dim, 0);
+			float occupancy = (maxActiveBlocks * y_dim / props.warpSize) / 
+				(float)(props.maxThreadsPerMultiProcessor / 
+						props.warpSize);
+			//printf("y_dim: %d, occ: %f\n", y_dim, occupancy);
+			
+			avg[i - 1] += t_kernel_end - t_kernel_start;
+			//printf("%d %lf\n", y_dim, t_kernel_end - t_kernel_start);
+			cudaCheckError();	
+		}
+	}
+
+	for(int i = 1; i <= 32; i++){
+		avg[i - 1] = avg[i - 1] / 10;
+		printf("%d %lf\n", i * 32, avg[i - 1]);
+	}
+		
+	/* - Allocate space on the host memory to store the result
 	   - Copy the result from device memory to host's
 	   - Print the points in the Skyline set 
 	*/
 	S = (int*) malloc((*N) * sizeof(int));
 	cudaSafeCall(cudaMemcpy(S, d_S, (*N) * sizeof(int), cudaMemcpyDeviceToHost));
-	for(int i = 0; i < *N; i++){
+	/*for(int i = 0; i < *N; i++){
 		if(S[i]){
 			for(int k = 0; k < *D; k++){
 				printf("%lf ", points[i * (*D) + k]);
 			}
 			printf("\n");
 		}
-	}
+	}*/
 
-	/* Print the time spent  by the kernel to determine the Skyline set */
-	float milliseconds = 0;
-	cudaEventElapsedTime(&milliseconds, t_kernel_start, t_kernel_stop);	
-	printf("%f\n", milliseconds / 1000);
+	/* Print the time spent by the kernel to determine the Skyline set */
+	//printf("%lf\n", t_kernel_end - t_kernel_start);
 	
 	/* Free space on device and host heap memory */
 	cudaFree(d_points);
