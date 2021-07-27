3892684dfa8f496306c465baeae591e19af3325d
w-disaster <luca.fabri1999@gmail.com>
2021-07-27 15:44:34 +0200

    add final versions, add README

--- src/final/cuda-skyline.cu
+++ src/final/cuda-skyline.cu
@@ -1,23 +1,62 @@
+/****************************************************************************
+ *
+ * cuda-skyline.cu - Skyline set computation with CUDA
+ *
+ * Author: Fabri Luca 
+ * Serial Number: 0000892878
+ * Email: luca.fabri@studio.unibo.it
+ * 
+ * ---------------------------------------------------------------------------
+ * 
+ * Skyline set computation with CUDA. 
+ * Given P a set of N points with dimension D, p1, p2 two points in P,
+ * we say that p1 dominates p2 if:
+ * - for each dimension k: p1[k] >= p2[k] , 0 <= k < D;
+ * - exists at least one dimension j such that: p1[j] > p2[j] , 0 <= j < D.
+ *
+ * The Skyline set is, for definition, composed of all points in P that aren't
+ * dominated by other points in P.
+ *
+ * Compile with:
+ * nvcc -Wno-deprecated-gpu-targets cuda-skyline.cu -o cuda-skyline -lm
+ * Or from Makefile:
+ * make cuda
+ *
+ * Run with:
+ * ./cuda-skyline < input_file > output_file
+ *
+ * Please not that the input_file provided as argument must contains:
+ * - The dimension of the points, in the first line. Next chars, if present, 
+ *   are ignored;
+ * - The number of points in the 2nd line;
+ * - The input set P whose points are in separed rows and each dimension value 
+ *   separated by space or tab.
+ *
+ ****************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include "lib/hpc.h"
+#include "hpc.h"
 
 #define LINE_LENGHT 4000
 #define WARP_SIZE 32
-#define MAX_DIM 200
 
-/* This function reads the points from a file descriptor and saves
+/* 
+ * This function reads the points from a file descriptor and saves
  * them into an array. Also, it stores the dimension D and
  * the number of points N onto two int memory locations.
- * Arguments:
+ * 
+ * Parameters:
  * - fd: file descriptor
  * - N: pointer to integer where this function stores the number of points read
  * - D: pointer to int where this function stores the dimension of the points.
+ * 
  * It returns the pointer to the allocated array containing the points.
  */
-double *read_points(FILE* fd, int* N, int* D){
+
+double* read_points(FILE* fd, int* N, int* D){
     char line[LINE_LENGHT];
     const size_t BUF_SIZE = sizeof(line);
 	    
@@ -25,18 +64,16 @@ double *read_points(FILE* fd, int* N, int* D){
     char* dim;
     dim = fgets(line, BUF_SIZE, fd);
     sscanf(dim, "%d", D);
-    printf("%d\n", *D);
     
     /* Read the number of points: fetch the second line until newline */
     char* n;
     n = fgets(line, BUF_SIZE, fd);
     sscanf(n, "%d", N);
-    printf("%d\n", *N);
-
-    /* Allocate the return array with N x D elements: points[0...D-1] contains
-       the 1st point, points[D...D*2-1] the 2nd and so on.
+    
+    /* Allocate the matrix (N x D), where each line i contains the values
+	   of the points on that dimension i.
 	*/
-    double *points = (double*) malloc((*N) * (*D) * sizeof(double));
+    double *matrix = (double*) malloc((*N) * (*D) * sizeof(double));
 	
     char* str;
     const char* s = " ";
@@ -48,31 +85,36 @@ double *read_points(FILE* fd, int* N, int* D){
         /* Split the string read on s=" " separator and fetch the values */
         token = strtok(str, s);
         for(int k = 0; k < *D && token != NULL; k++){
-            /* convert ASCII string to floating-point number */
-            points[i * (*D) + k] = strtod(token, &ptr);
+            /* convert ASCII string to doubleing-point number */
+            matrix[k * (*N) + i] = strtod(token, &ptr);
             token = strtok(NULL, s);
         }
     }
-    return points;
+    return matrix;
 }
 
 /* Returns true if the array s dominates the array d. 
  * Parameters:
  * - s, d: arrays of double
- * - length: number of elements of s and d 
+ * - dim: number of elements of s and d
+ * - offset: distance between two elements that we must read in array s, d
  */
-__device__ bool dominance(double *s, double *d, int length){
-    bool strictly_minor = false;
+__device__ bool dominance(double *s, double *d, int dim, int offset){
     bool strictly_major = false;
-    for(int i = 0; i < length && !strictly_minor; i++){
-        if(s[i] < d[i]){
-			 strictly_minor = true;
+    /* Iterate over each index: 
+     * if s[i] < d[i] then s doesn't dominate d --> return */
+    for(int i = 0; i < dim; i++){
+        if(s[i * offset] < d[i * offset]){
+			 return false;
 		}
-        if(s[i] > d[i]){
+        if(s[i * offset] > d[i * offset]){
 			strictly_major = true;
 		}
     }
-    return !strictly_minor && strictly_major;
+    /* If there aren't elements strictly minor and exist at least on element
+     * strictly major then s dominates d
+     */
+    return strictly_major;
 }
 
 /* Kernel function:
@@ -81,92 +123,101 @@ __device__ bool dominance(double *s, double *d, int length){
  * if any of them dominates it.
  * The result, in the end, is put in the array S, stored in the global memory. 
  */
-__global__ void compute_skyline(double *points, bool *S, int n, int d){
+__global__ void compute_skyline(double *points, bool *S, int *k, int n, int d){
 	const int y = blockIdx.y * blockDim.y + threadIdx.y;
 	if(y < n){
-		/* Copy the number in charge to the local memory in order
-		   to perform coalesced memory accesses
-		*/
-		double num[MAX_DIM];
-		memcpy(num, &points[y * d], d * sizeof(double));
-
-		int is_skyline_point = true;
+		bool is_skyline_point = true;
 		for(int i = 0; i < n && is_skyline_point; i++){
 			/* If num is dominates by another number then it is not
 			   in the Skyline set
 			*/
 			if(i != y){
-				if(dominance(&points[i * d], num, d)){
+				if(dominance(&points[i], &points[y], d, n)){
 					is_skyline_point = false;						 
 				}
 			}
 		}
 		/* Copy the results on the device global memory */
 		S[y] = is_skyline_point;
+        /* Increment by 1 the cardinality of Skyline set if the number 
+           is in the Skyline set, by 0 otherwise 
+         */
+        atomicAdd(k, is_skyline_point); 
 	}
 }
 
-int main(int argc, char *argv[]){
-   	/* Allocate memory to store the number of points, the dimension and the number of points */
-	int *D = (int*) malloc(sizeof(int));
-    int *N = (int*) malloc(sizeof(int));
-    double *points = read_points(stdin, N, D);
+/*
+ * This function prints to the file descriptor fd given as parameter:
+ * - The dimension D of the points;
+ * - The cardinality K of the Skyline set;
+ * - The Skyline set.
+ */
+__host__ void print_skyline(FILE* fd, bool *S, double *points, int N, int D, int K){
+    int i, j;
+    /* Print D, K */
+    fprintf(fd, "%d\n%d\n", D, K);
 
+    /* Print the Skyline set */
+    for(i = 0; i < N; i++){
+        if(S[i]){
+            for(j = 0; j < D; j++){
+                fprintf(fd, "%f ", points[j * N + i]);
+            }
+            fprintf(fd, "\n");
+        }
+    }
+}
+
+int main(void){
+	/* Allocate memory to store the number of points, them dimension and the points */
+	int* D = (int*) malloc(sizeof(int));
+    int* N = (int*) malloc(sizeof(int));
+
+	double* points = read_points(stdin, N, D);
+   
 	/* - Define the matrix dimension, 
 	   - Allocate space on the device global memory 
 	   - Copy the array points on the allocated space
 	 */
 	const size_t size = (*N) * (*D) * sizeof(double);
-    double *d_points;
+    double* d_points;
 	cudaSafeCall(cudaMalloc((void**)&d_points, size));
 	cudaSafeCall(cudaMemcpy(d_points, points, size, cudaMemcpyHostToDevice));
 
 	/* Allocate space where the kernel function will store the result */
 	bool *S, *d_S;
-	cudaSafeCall(cudaMalloc((void**)&d_S, (*N) * sizeof(bool)));	
+	cudaSafeCall(cudaMalloc((void**)&d_S, (*N) * sizeof(bool)));
+
+	/* Allocate space in order to store the cardinality of the Skyline set */
+    int *K, *d_K;
+    K = (int*) malloc(sizeof(int));
+    *K = 0;
+    cudaSafeCall(cudaMalloc((void**)&d_K, sizeof(int)));    
+    cudaSafeCall(cudaMemcpy(d_K, K, sizeof(int), cudaMemcpyHostToDevice));
 
 	/* Define the block and grid dimensions */
 	dim3 block(1, WARP_SIZE * 2);
 	dim3 grid(1, ((*N) + WARP_SIZE * 2 - 1)/(WARP_SIZE * 2));
-		
-	/* - Kernel function call to determine the Skyline set
-	   - Calculate the time spent 
-	   - Check for errors occurred in the GPU
-	*/
-	cudaEvent_t t_kernel_start, t_kernel_stop;
-	cudaEventCreate(&t_kernel_start);
-	cudaEventCreate(&t_kernel_stop);	
+
+	/* Kernel function call to determine the Skyline set */
+	compute_skyline<<<grid, block>>>(d_points, d_S, d_K, *N, *D);
 	
-	cudaEventRecord(t_kernel_start);
-    compute_skyline<<<grid, block>>>(d_points, d_S, *N, *D);
-	cudaEventRecord(t_kernel_stop);
+	/* While Kernel function is executing on device, allocate memory on heap 
+	 * in order to store the result 
+     */
+	S = (bool*) malloc((*N) * sizeof(bool));
+	
+	/* Wait the Kernel to finish and check errors */
 	cudaCheckError();	
-	
-	/* - Allocate space on heap (host memory) in order to store the result
-	   - Copy the result from device memory to host's
+
+	/* - Copy the result from device memory to host's
+       - Copy the Skyline cardinality from device to host memory
 	   - Print the points in the Skyline set 
 	*/
-	S = (bool*) malloc((*N) * sizeof(bool));
 	cudaSafeCall(cudaMemcpy(S, d_S, (*N) * sizeof(bool), cudaMemcpyDeviceToHost));
-	for(int i = 0; i < *N; i++){
-		if(S[i]){
-			for(int k = 0; k < *D; k++){
-				printf("%lf ", points[i * (*D) + k]);
-			}
-			printf("\n");
-		}
-	}
+    cudaSafeCall(cudaMemcpy(K, d_K, sizeof(int), cudaMemcpyDeviceToHost));
+    print_skyline(stdout, S, points, *N, *D, *K);
 
-	/* Print the time spent  by the kernel to determine the Skyline set */
-	float milliseconds = 0;
-	cudaEventElapsedTime(&milliseconds, t_kernel_start, t_kernel_stop);	
-	printf("%f\n", milliseconds / 1000);
-	
-	/* Free space on device and host heap memory */
-	cudaFree(d_points);
-	free(points);
-	free(S);
-	free(D);
-	free(N);
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
+
--- src/final/omp-skyline.c
+++ src/final/omp-skyline.c
@@ -1,19 +1,58 @@
+/****************************************************************************
+ *
+ * omp-skyline.c - Skyline set computation with OpenMP
+ *
+ * Author: Fabri Luca 
+ * Serial Number: 0000892878
+ * Email: luca.fabri@studio.unibo.it
+ * 
+ * ---------------------------------------------------------------------------
+ * 
+ * Skyline set computation with OpenMP. 
+ * Given P a set of N points with dimension D, p1, p2 two points in P,
+ * we say that p1 dominates p2 if:
+ * - for each dimension k: p1[k] >= p2[k] , 0 <= k < D;
+ * - exists at least one dimension j such that: p1[j] > p2[j] , 0 <= j < D.
+ *
+ * The Skyline set is, for definition, composed of all points in P that aren't
+ * dominated by other points in P.
+ *
+ * Compile with:
+ * gcc -std=c99 -Wall -Wpedantic -O2 -D_XOPEN_SOURCE=600 -fopenmp \
+ * 		omp-skyline.c -lm -o omp-skyline
+ * Or from Makefile:
+ * make openmp
+ *
+ * Run with:
+ * ./omp-skyline < input_file > output_file
+ *
+ * Please not that the input_file provided as argument must contains:
+ * - The dimension of the points, in the first line. Next chars, if present, 
+ *   are ignored;
+ * - The number of points in the 2nd line;
+ * - The input set P whose points are in separed rows and each dimension value 
+ *   separated by space or tab.
+ *
+ ****************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 #include <omp.h>
-#include "lib/hpc.h"
 
 #define LINE_LENGHT 4000
 
-/* This function reads the points from a file descriptor and saves
+/* 
+ * This function reads the points from a file descriptor and saves
  * them into a matrix. Also, it stores the dimension D and
  * the number of points N onto two int memory locations.
+ * 
  * Parameters:
  * - fd: file descriptor
  * - N: pointer to integer where this function stores the number of points read
  * - D: pointer to int where this function stores the dimension of the points.
+ * 
  * It returns the double pointer to the allocated matrix containing the points.
  */
 double **read_points(FILE *fd, int *N, int *D){
@@ -24,14 +63,12 @@ double **read_points(FILE *fd, int *N, int *D){
     char *dim;
     dim = fgets(line, BUFSIZE, fd);
     sscanf(dim, "%d", D);
-    printf("%d\n", *D);
     
     /* Read the number of points: fetch the second line until newline */
     char *n;
     n = fgets(line, BUFSIZE, fd);
     sscanf(n, "%d", N);
-    printf("%d\n", *N);
-
+    
     /* Allocate the return matrix of dimension N x D where each line contains
      * the coordinates of a point. 
      */
@@ -48,7 +85,7 @@ double **read_points(FILE *fd, int *N, int *D){
         /* Split the string read on s=" " separator and fetch the values */
         token = strtok(str, s);
         for(int k = 0; k < *D && token != NULL; k++){
-            /* convert ASCII string to floating-point number */
+            /* convert ASCII string to doubleing-point number */
             points[i][k] = strtod(token, &ptr);
             token = strtok(NULL, s);
         }
@@ -56,49 +93,66 @@ double **read_points(FILE *fd, int *N, int *D){
     return points;
 }
 
-/* Returns true if the array s dominates the array d. 
+/* 
+ * Returns true if the array s dominates the array d. 
  * Parameters:
  * - s, d: arrays of double
- * - dim: number of elements of s and d 
+ * - length: number of elements of s and d 
  */
-bool dominance(double *s, double *d, int dim){
-    bool strictly_minor = false;
+bool dominance(double *s, double *d, int length){
     bool strictly_major = false;
-    for(int i = 0; i < dim && !strictly_minor; i++){
+    /* Iterate over each index: 
+     * if s[i] < d[i] then s doesn't dominate d --> return */
+    for(int i = 0; i < length; i++){
         if(s[i] < d[i]){
-			strictly_minor = true;
+			return false;
 		}
         if(s[i] > d[i]){
 			strictly_major = true;
 		}
     }
-    return !strictly_minor && strictly_major;
+    /* If there aren't elements strictly minor and exist at least on element
+     * strictly major then s dominates d
+     */
+    return strictly_major;
 }
 
-/* This function computes the Skyline set, Given:
- * - points, a matrix containing the points
- * - rows, the number of points
- * - cols, the dimension of the points
+/* 
+ * This function computes the Skyline set, Given:
+ * - points, a matrix containing the points;
+ * - rows, the number of points;
+ * - cols, the dimension of the points;
+ * - skyline_length, pointer to int to store the cardinality 
+ *   of the Skyline set.
+ *
  * Returns an array of rows booleans where array[i] == true, 0 <= i < rows, 
  * if the i-th element is in the Skyline set, array[i] == false otherwise.
  */
-bool* compute_skyline(double **points, int rows, int cols){
+bool* compute_skyline(double **points, int rows, int cols, int *skyline_card){
     bool *S = (bool*) malloc(rows * sizeof(bool)); 
     int n_threads = omp_get_max_threads();
     int i, j;
-    
+    int S_length[n_threads];
+    *skyline_card = 0;
+	
     /* This section creates a pool of threads:
      * each one compares the assigned points (of its subset) with all the others
      * in the set. If a point in the set is dominated by one of the subset then
      * we assign false into the corrisponding element of the return array to
      * indicate that it's not in the Skyline set.
+     * The writes on the array S are atomic because it's shared between the
+     * threads and we must avoid race conditions.
+     * When all threads exit the external for the cardinality of the Skyline set
+     * is computed.
      */
-#pragma omp parallel default(none) num_threads(n_threads) private(i, j) shared(S, rows, cols, points, n_threads)
+#pragma omp parallel default(none) num_threads(n_threads) private(i, j) \
+    shared(S, S_length, skyline_card, rows, cols, points, n_threads)
     {   
-        /* Compute local start and local end indexes, initialize array S*/ 
+        /* Compute local start and local end indexes, initialize array S */ 
         int thread_id = omp_get_thread_num();
         int local_start = rows * thread_id / n_threads;
         int local_end = rows * (thread_id + 1) / n_threads;
+		S_length[thread_id] = 0;
         for(i = local_start; i < local_end; i++) S[i] = true;
 #pragma omp barrier    
         /* Once S is full initialized, start computing Skyline set */
@@ -106,38 +160,62 @@ bool* compute_skyline(double **points, int rows, int cols){
             if(S[i]){
                 for(j = 0; j < rows; j++){
                     if(S[j] && dominance(points[i], points[j], cols)){
-#pragma omp critical
-                        S[j] = false;
+#pragma omp atomic write 
+                        S[j] = false;            
                     }
                 } 
             }
         }
+#pragma omp barrier
+        /* Each thread calculates a partial cardinality of Skyline set */
+        for(i = local_start; i < local_end; i++) {
+            if(S[i]){
+                S_length[thread_id]++;
+            }
+        }
+#pragma omp atomic
+        *skyline_card += S_length[thread_id];
     }
     return S;
 }
 
-int main(int argc, char* argv[]){
+/*
+ * This function prints to the file descriptor fd given as parameter:
+ * - The dimension D of the points;
+ * - The cardinality K of the Skyline set;
+ * - The Skyline set.
+ */
+void print_skyline(FILE* fd, bool *S, double **points, int N, int D, int K){
+    int i, j;
+    /* Print D, K */
+    fprintf(fd, "%d\n%d\n", D, K);
+
+    /* Print the Skyline set */
+    for(i = 0; i < N; i++){
+        if(S[i]){
+            for(j = 0; j < D; j++){
+                fprintf(fd, "%lf ", points[i][j]);
+            }
+            fprintf(fd, "\n");
+        }
+    }
+}
+
+int main(void){
+    /* int pointers to store dimension D, cardinality N of the input set
+     * and Skyline cardinality K.
+     */
     int *D = (int*) malloc(sizeof(int));
     int *N = (int*) malloc(sizeof(int));
+    int *K = (int*) malloc(sizeof(int));
+
     /* Read the points from stdin */
     double **points = read_points(stdin, N, D);
 
     /* Calculate the Skyline set and measure time spent */
-    double t_start = omp_get_wtime();
-    bool *skyline = compute_skyline(points, *N, *D);
-    double t_end = omp_get_wtime();
-
-    /* Print the Skyline set and the time spent */
-    int i, j;
-    for(i = 0; i < *N; i++){
-        if(skyline[i]){
-            for(j = 0; j < *D; j++){
-                printf("%lf ", points[i][j]);
-            }
-            printf("\n");
-        }
-    }
-    printf("Time: %lf\n", t_end - t_start);
-    
+    bool *skyline = compute_skyline(points, *N, *D, K);
+   
+	/* Print Skyline set */
+	print_skyline(stdout, skyline, points, *N, *D, *K);	
     return EXIT_SUCCESS;
 }
